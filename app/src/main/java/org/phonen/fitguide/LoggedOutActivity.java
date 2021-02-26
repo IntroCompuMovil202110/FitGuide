96addfefa46324653a83c34e8da33256b7a94b67
Daniela Cortes <dcortesantonio@gmail.com>
2021-02-25 23:15:59 -0500

    NavBar added

--- app/src/main/java/org/phonen/fitguide/LoggedOutActivity.java
+++ app/src/main/java/org/phonen/fitguide/LoggedOutActivity.java
@@ -2,13 +2,23 @@ package org.phonen.fitguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoggedOutActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_out);
+        button = findViewById(R.id.button);
+
+
+    }
+
+    public void Profile(View view) {
+        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
     }
 }
