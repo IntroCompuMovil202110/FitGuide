c8826d82d84e0243cff5b683fdd293716f68d824
Daniela Cortes <dcortesantonio@gmail.com>
2021-03-01 10:58:47 -0500

    Intents added

--- app/src/main/java/org/phonen/fitguide/AddFriendActivity.java
+++ app/src/main/java/org/phonen/fitguide/AddFriendActivity.java
@@ -1,14 +1,56 @@
 package org.phonen.fitguide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AddFriendActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+        navBarSettings();
+    }
+    public void navBarSettings(){
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.profileActivity);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.challengesActivity:
+                        startActivity(new Intent(getApplicationContext(), ChallengesActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.feedActivity:
+                        startActivity(new Intent(getApplicationContext(), FeedActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.profileActivity:
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.startActivity:
+                        startActivity(new Intent(getApplicationContext(), StartActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+    public void AddFriend(View view) {
+        startActivity(new Intent(getApplicationContext(), AddFriendActivity.class));
+    }
+
+    public void Contacts(View view) {
+        startActivity(new Intent(getApplicationContext(), ContactsActivity.class));
     }
 }
--- app/src/main/java/org/phonen/fitguide/LoggedOutActivity.java
+++ app/src/main/java/org/phonen/fitguide/LoggedOutActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +18,12 @@ public class LoggedOutActivity extends AppCompatActivity {
 
     }
 
-    public void Profile(View view) {
-        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+
+    public void Login(View view) {
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+    public void Register(View view) {
+        startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
     }
 }
