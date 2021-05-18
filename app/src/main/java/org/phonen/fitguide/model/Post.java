a6c2ef2eb06057e526a5cf4ec9c5b669c3b6ad6d
Nicolás <ncp.434@hotmail.com>
2021-05-18 13:08:12 -0500

    Post DTO added

--- app/src/main/java/org/phonen/fitguide/model/Post.java
+++ app/src/main/java/org/phonen/fitguide/model/Post.java
@@ -1,8 +1,54 @@
 package org.phonen.fitguide.model;
 
+import java.util.Date;
+
 public class Post {
     private int type;
     private String userUID;
     private String description;
     private String imagePath;
+    private Date date;
+
+    public Post() {
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getUserUID() {
+        return userUID;
+    }
+
+    public void setUserUID(String userUID) {
+        this.userUID = userUID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
