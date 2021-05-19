1c0befd2f6a22f4837b9fc71b16e7a0b18349e61
Mateo Florido <mateoflosan@gmail.com>
2021-05-18 22:40:54 -0500

    Added Adapter and Image Cache

--- app/src/main/java/org/phonen/fitguide/helpers/FeedAdapter.java
+++ app/src/main/java/org/phonen/fitguide/helpers/FeedAdapter.java
@@ -1,57 +1,95 @@
 package org.phonen.fitguide.helpers;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.phonen.fitguide.R;
 import org.phonen.fitguide.model.Post;
 import org.phonen.fitguide.model.User;
 
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FeedAdapter extends ArrayAdapter<Post> {
 
-    List<User> users;
-    List<String> uids;
+    private Map<String, User> users;
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+    private Map<String, Bitmap> cachedImages;
+    private String uid;
 
-    public FeedAdapter(@NonNull Context context, List<Post> posts, List<User> users, List<String> uids) {
+    public FeedAdapter(@NonNull Context context, List<Post> posts, Map<String, User> users) {
         super(context, 0, posts);
         this.users = users;
-        this.uids = uids;
+        this.uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        this.cachedImages = new HashMap<>();
+
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Post post = getItem(position);
-        if(convertView == null){
+        if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.feed_item, parent, false);
         }
+
 
         TextView name = convertView.findViewById(R.id.feed_name);
         TextView username = convertView.findViewById(R.id.feed_username);
         TextView time = convertView.findViewById(R.id.feed_time);
         TextView description = convertView.findViewById(R.id.feed_text);
+        ImageView image = convertView.findViewById(R.id.feed_image_post);
 
-        int index = uids.indexOf(post.getUserUID());
-        if(index != -1){
-            User user = users.get(index);
-            name.setText(user.getName() + user.getLastName());
-            username.setText(user.getLevel());
-            // time.setText(post.getType());
-            switch (post.getType()){
-                case 0:
-                    description.setText("Ha completado un nuevo ejercicio! Ha quemado " + post.getDescription() + " calorias!");
-                    break;
+
+        User user = users.get(post.getUserUID());
+        name.setText(user.getName() + " " + user.getLastName());
+        username.setText(user.getUserName());
+        time.setText(format.format(post.getDate()));
+        if(!post.getImagePath().isEmpty()){
+            if(cachedImages.get(post.getImagePath()) == null){
+                StorageReference imageRef = FirebaseStorage.getInstance().getReference();
+                imageRef.child(post.getImagePath())
+                        .getBytes(5 * 1024 * 1024).addOnSuccessListener(v -> {
+                    Bitmap bm = BitmapFactory.decodeByteArray(v, 0, v.length);
+                    image.setImageBitmap(bm);
+                    cachedImages.put(post.getImagePath(), bm);
+                });
+            }else {
+                Log.i("CACHE HIT", "Already cached image, using this one");
+                image.setImageBitmap(cachedImages.get(post.getImagePath()));
             }
+        }
+
+        LinearLayout layoutTop = convertView.findViewById(R.id.feed_top_card);
+        if(post.getUserUID().equals(uid)){
+            layoutTop.setBackground(convertView.getResources().getDrawable(R.drawable.feed_own_user, getContext().getTheme()));
+        } else {
+            layoutTop.setBackground(convertView.getResources().getDrawable(R.drawable.border_feed_message, getContext().getTheme()));
+        }
+        switch (post.getType()) {
+            case 0:
+                description.setText(post.getDescription());
+                break;
         }
 
         return convertView;
